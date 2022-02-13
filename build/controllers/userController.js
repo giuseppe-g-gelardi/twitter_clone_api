613302e1acada3824120575a1775a52f3bfe5101
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2022-02-13 01:07:15 -0600

    update controller functions, add get user by username

--- build/controllers/userController.js
+++ build/controllers/userController.js
@@ -15,51 +15,74 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.register = exports.getUserByUsername = exports.getUsers = void 0;
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const userModel_1 = __importDefault(require("../models/userModel"));
-const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const user = yield userModel_1.default.find();
-        return res.json(user);
-    }
-    catch (error) {
-        throw new Error('unable to get users');
-    }
-});
+function getUsers(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return userModel_1.default.find()
+            .then(user => {
+            if (!user)
+                return res.status(404).json('Users not found');
+            return res.json(user);
+        })
+            .catch(error => res.status(500).send(error));
+    });
+}
 exports.getUsers = getUsers;
-const getUserByUsername = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const user = yield userModel_1.default.find({ username: req.params.username });
-        if (!user)
-            res.status(403).json(`User ${user} not found`);
-        return res.status(200).json(user);
-    }
-    catch (error) {
-    }
-});
+function getUserByUsername(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        userModel_1.default.findOne({ username: req.params.username })
+            .then(user => {
+            if (!user)
+                return res.status(404).json('user not found');
+            return res.json(user);
+        })
+            .catch(error => res.status(500).json(error));
+    });
+}
 exports.getUserByUsername = getUserByUsername;
-const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let user = yield userModel_1.default.findOne({ email: req.body.email });
-        if (user)
-            return res.status(400).send('User already registered.');
-        const salt = yield bcryptjs_1.default.genSalt(10);
-        user = new userModel_1.default({
-            username: req.body.username,
-            email: req.body.email,
-            password: yield bcryptjs_1.default.hash(req.body.password, salt),
-        });
-        yield user.save();
-        const token = user.generateAuthToken();
-        // const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT);
-        return res
-            .header('x-auth-token', token)
-            .header('access-control-expose-headers', 'x-auth-token')
-            .send({ _id: user._id, username: user.username, email: user.email });
-    }
-    catch (error) {
-        throw new Error('trouble making a new user');
-    }
-});
+function register(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        userModel_1.default.findOne({ email: req.body.email })
+            .then((user) => __awaiter(this, void 0, void 0, function* () {
+            if (user)
+                return res.status(400).json('User already exists');
+            const salt = yield bcryptjs_1.default.genSalt(10);
+            user = new userModel_1.default({
+                username: req.body.username,
+                email: req.body.email,
+                password: yield bcryptjs_1.default.hash(req.body.password, salt),
+            });
+            yield user.save();
+            const token = user.generateAuthToken(user._id);
+            return res
+                .header('x-auth-token', token)
+                .header('access-control-expose-headers', 'x-auth-token')
+                .send({ _id: user._id, username: user.username, email: user.email });
+        }))
+            .catch(error => res.status(500).json(error));
+    });
+}
 exports.register = register;
+// export const registera = async (req: Request, res: Response) => {
+//   try {
+//     let user = await User.findOne({ email: req.body.email })
+//     if (user) return res.status(400).send('User already registered.')
+//     const salt = await bcrypt.genSalt(10)
+//     user = new User({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: await bcrypt.hash(req.body.password, salt),
+//     })
+//     await user.save()
+//     const token = user.generateAuthToken()
+//     // const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT);
+//       return res
+//       .header('x-auth-token', token)
+//       .header('access-control-expose-headers', 'x-auth-token')
+//       .send({ _id: user._id, username: user.username, email: user.email });
+//   } catch (error) {
+//     throw new Error('trouble making a new user')
+//   }
+// }
 // const userExists = await User.findOne({ email: req.body.email })
 // if (userExists) return res.status(400).json(`Account with email ${req.body.email} already exists`)
 // const salt = await bcrypt.genSalt(10)
@@ -75,3 +98,18 @@ exports.register = register;
 // .header('x-auth-token', token)
 // .header('access-control-expose-headers', 'x-auth-token')
 // .send({ _id: user._id, username: user.username, email: user.email });
+// export const getUsers = async (req: Request, res: Response) => {
+// try {
+//   const user = User.find()
+//   return res.json(user)
+// } catch (error) {
+//   throw new Error('unable to get users')
+// }
+// export const getUserByUsername = async (req: Request, res: Response) => {
+//   try {
+//     const user = await User.find({ username: req.params.username })
+//     if (!user) res.status(403).json(`User ${user} not found`)
+//     return res.status(200).json(user)
+//   } catch (error) {
+//   }
+// }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,51 +3,71 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import User from '../models/userModel'
 
-export const getUsers = async (req: Request, res: Response) => {
-  try {
-    const user = await User.find()
-    return res.json(user)
-  } catch (error) {
-    throw new Error('unable to get users')
-  }
-}
-
-export const getUserByUsername = async (req: Request, res: Response) => {
-  try {
-    const user = await User.find({ username: req.params.username })
-    if (!user) res.status(403).json(`User ${user} not found`)
-
-    return res.status(200).json(user)
-    
-  } catch (error) {
-    
-  }
-}
-
-export const register = async (req: Request, res: Response) => {
-  try {
-    let user = await User.findOne({ email: req.body.email })
-    if (user) return res.status(400).send('User already registered.')
-
-    const salt = await bcrypt.genSalt(10)
-    user = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, salt),
+export async function getUsers(req: Request, res: Response) {
+  return User.find()
+    .then(user => {
+      if (!user) return res.status(404).json('Users not found')
+      return res.json(user)
     })
+    .catch(error => res.status(500).send(error))
+}
 
-    await user.save()
+export async function getUserByUsername(req: Request, res: Response) {
+    User.findOne({ username: req.params.username })
+    .then(user => {
+      if (!user) return res.status(404).json('user not found')
+      return res.json(user)
+    })
+    .catch(error => res.status(500).json(error))
+}
 
-    const token = user.generateAuthToken()
-    // const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT);
+export async function register(req: Request, res: Response) {
+  User.findOne({ email: req.body.email })
+    .then(async user => {
+      if (user) return res.status(400).json('User already exists')
+
+      const salt = await bcrypt.genSalt(10)
+      user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, salt),
+      })
+      await user.save()
+
+      const token = user.generateAuthToken(user._id)
+
       return res
       .header('x-auth-token', token)
       .header('access-control-expose-headers', 'x-auth-token')
       .send({ _id: user._id, username: user.username, email: user.email });
-  } catch (error) {
-    throw new Error('trouble making a new user')
-  }
+    })
+    .catch(error => res.status(500).json(error))
 }
+
+// export const registera = async (req: Request, res: Response) => {
+//   try {
+//     let user = await User.findOne({ email: req.body.email })
+//     if (user) return res.status(400).send('User already registered.')
+
+//     const salt = await bcrypt.genSalt(10)
+//     user = new User({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: await bcrypt.hash(req.body.password, salt),
+//     })
+
+//     await user.save()
+
+//     const token = user.generateAuthToken()
+//     // const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT);
+//       return res
+//       .header('x-auth-token', token)
+//       .header('access-control-expose-headers', 'x-auth-token')
+//       .send({ _id: user._id, username: user.username, email: user.email });
+//   } catch (error) {
+//     throw new Error('trouble making a new user')
+//   }
+// }
 
 
 
@@ -70,3 +90,24 @@ export const register = async (req: Request, res: Response) => {
 // .header('x-auth-token', token)
 // .header('access-control-expose-headers', 'x-auth-token')
 // .send({ _id: user._id, username: user.username, email: user.email });
+
+
+
+
+// export const getUsers = async (req: Request, res: Response) => {
+  // try {
+  //   const user = User.find()
+  //   return res.json(user)
+  // } catch (error) {
+  //   throw new Error('unable to get users')
+  // }
+
+  // export const getUserByUsername = async (req: Request, res: Response) => {
+//   try {
+//     const user = await User.find({ username: req.params.username })
+//     if (!user) res.status(403).json(`User ${user} not found`)
+
+//     return res.status(200).json(user)
+//   } catch (error) {
+//   }
+// }
