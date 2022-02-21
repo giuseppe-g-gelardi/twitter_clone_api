56cb882b2a3070165dee33a5c0bcc37bdd3961e2
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2022-02-21 02:00:57 -0600

    connect go server to mongodb

--- services/main.go
+++ services/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // type User struct {
@@ -17,11 +23,31 @@ import (
 // }
 
 func init() {
+	// * load .env file
 	err := godotenv.Load("../.env")
-	if err != nil { 
-		fmt.Println("Error loading .env")
+	if err != nil {fmt.Println("Error loading .env")}
+	fmt.Println(".env successfully loaded")
+
+	// * connect to database
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DB")))
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println("JWT Secret Key:", os.Getenv("JWT"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 }
 
 func main() {
@@ -30,9 +56,9 @@ func main() {
 
 func server() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello... from go ;) ")
+		fmt.Fprintf(w, "Hello... from go! :D ")
 	})
 
-	fmt.Println("JWT Auth server started on port 8080 ")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started on port", os.Getenv("SERVICE_PORT"))
+	log.Fatal(http.ListenAndServe(os.Getenv("SERVICE_PORT"), nil))
 }
